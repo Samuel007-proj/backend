0ac159a0f2831a297af42b3173be7795388f3650
Samuel Oyelami <oyelamisamuel31@gmail.com>
2022-07-03 18:33:35 +0100

    backend v1

---
+++ index.js
@@ -0,0 +1,172 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+//middleware
+const reqLogger = (req, resp, next) =>{
+  console.log('Method:', req.method)
+  console.log('Path: ', req.path)
+  console.log('Body: ', req.body)
+  console.log('------')
+  next()
+
+}
+
+
+app.use(reqLogger)
+app.use(morgan('combined'))
+
+morgan.token('type', function (req, res) { return req.headers['content-type'] })
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2022-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-05-30T19:20:14.298Z",
+      important: true
+    }
+  ];
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+app.get('/', (req, resp) => {
+    resp.send('<h1>Hello nodemon</h1>')
+})
+
+app.get('/api/notes/', (req, resp)=>{
+  resp.send(notes)
+})
+
+app.get('/api/notes/:id', (req, resp)=>{
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    if(note){
+       resp.json(note)
+    }else{
+        resp.status(404).end()
+    }
+    
+})
+app.delete('/api/notes/:id', (req, resp) =>{
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    resp.status(204).end()
+})
+
+const generateId = ()=>{
+  const maxId = notes.length >0 
+    ? Math.max(...notes.map(n=>n.id))
+    : 0;
+    return maxId+1;
+  } 
+
+app.post('/api/notes/', (req, resp)=>{
+  const body = req.body;
+
+  if(!body.content){
+    return resp.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date().toISOString(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+  resp.json(note)
+})
+
+//Phonebook
+
+app.get('/api/persons', (req, resp)=>{
+  resp.send(persons)
+})
+
+app.get('/api/persons/:id', (req, resp)=>{
+  const id  = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if(person){
+    resp.send(`
+      <h4>Name: ${person.name}</h4>
+      <p>Number: ${person.number}</p>
+      <p>Id: ${person.id}</p>
+    `)
+  }else{resp.send('<h1>Contact Not Found</h1>')}
+})
+
+app.get('/info', (req, resp)=>{
+  resp.send(`<h4>Phonebook has info for ${persons.length} people</h4>
+  <p>${new Date().toString()}</p>`);
+})
+
+app.delete('/api/persons/:id', (req, resp)=>{
+  const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    
+    resp.send(`<h4>deletion successful</h4>`).end()
+})
+
+app.post('/api/persons', (req, resp)=>{
+  let body  = req.body;
+
+  if(!(body.name && body.number)){
+    resp.status(500).send(`<h5>The or number is missing</h5>`)
+    resp.end();
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random()*100)
+  }
+  persons = persons.concat(person);
+  resp.json(person);
+})
+
+
+const PORT = process.env.PORT ||3001;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
